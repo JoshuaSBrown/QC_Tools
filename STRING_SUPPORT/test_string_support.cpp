00bf745cd499c806e224ca3ad374397b3c406450
Joshua S Brown <joshbro42867@yahoo.com>
2018-02-17 15:11:34 +0100

    Extended testing of string support functions

--- STRING_SUPPORT/test_string_support.cpp
+++ STRING_SUPPORT/test_string_support.cpp
@@ -3,17 +3,31 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
 int main(void){
   
-  string str = "Hello World    Lot of space";
-  vector<string> v_str = splitSt(str);
-
-  for(string t_str : v_str ){
-    cout << t_str << endl;
+  cerr << "Testing: splitSt" << endl;
+  {
+    string str = "Hello World    Lot of space";
+    vector<string> v_str = splitSt(str);
+    assert(v_str.at(0).compare("Hello")==0);
+    assert(v_str.at(1).compare("World")==0);
+    assert(v_str.at(2).compare("Lot")==0);
+    assert(v_str.at(3).compare("of")==0);
+    assert(v_str.at(4).compare("space")==0);
+    
   }
+
+  cerr << "Testing: lastN" << endl;
+  {
+    string str = "Hi there!";
+    string var = lastN(str,3);
+    assert(var.compare("re!")==0);
+  }
+  
 
   return 0;
 }
