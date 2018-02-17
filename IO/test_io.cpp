f7e792e760ce5cd3f1bee58f0b0af1cf8fce0457
Joshua S Brown <joshbro42867@yahoo.com>
2018-02-17 22:54:43 +0100

    Added test for counting MO

--- IO/test_io.cpp
+++ IO/test_io.cpp
@@ -1,19 +1,29 @@
 
 #include "io.hpp"
+
 #include <string>
 #include <vector>
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
 int main(void){
   
-  string str = "Hello World    Lot of space";
-  vector<string> v_str = splitSt(str);
-
-  for(string t_str : v_str ){
-    cout << t_str << endl;
+  cerr << "Testing: fileExt" << endl;
+  {
+    string filePath = "Dir1/Dir2/file4.xt";
+    string ext = fileExt(filePath);
+    assert(ext.compare(".xt")==0);    
   }
 
+  cerr << "Testing: pun_countMO" << endl;
+  {
+    string path = "../GAUSSIANFILES/90_u/90_pair.pun";
+    int MO = pun_countMO(path,"Beta");
+    assert(MO==92);
+    MO = pun_countMO(path,"Alpha");
+    assert(MO==92);
+  }
   return 0;
 }
