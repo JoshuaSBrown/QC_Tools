592db76fe9f33f6f3e21c99215b5276404ee929b
JoshuaSBrown <joshbro42867@yahoo.com>
2018-07-13 13:09:46 -0600

    Corrected test script for untaring files

--- src/test_script_calc_J.sh
+++ src/test_script_calc_J.sh
@@ -69,9 +69,9 @@ fi
 echo $data >> $fileOut
 
 # Because they are big files they are stored in tar.gz format we must first untar them
-tar -xzf ${path}GAUSSIANFILES/P_test/A.tar.gz -C ${path}GAUSSIANFILES/P_test/
-tar -xzf ${path}GAUSSIANFILES/P_test/B.tar.gz -C ${path}GAUSSIANFILES/P_test/
-tar -xzf ${path}GAUSSIANFILES/P_test/AB.tar.gz -C ${path}GAUSSIANFILES/P_test/
+tar -xzf ${path}/GAUSSIANFILES/P_test/A.tar.gz -C ${path}/GAUSSIANFILES/P_test/
+tar -xzf ${path}/GAUSSIANFILES/P_test/B.tar.gz -C ${path}/GAUSSIANFILES/P_test/
+tar -xzf ${path}/GAUSSIANFILES/P_test/AB.tar.gz -C ${path}/GAUSSIANFILES/P_test/
 exec_command="${path}/build/calc_J -p_P ${path}/GAUSSIANFILES/P_test/AB.pun  -p_1 ${path}/GAUSSIANFILES/P_test/A.pun -p_2 ${path}/GAUSSIANFILES/P_test/B.pun"
 data=$(${exec_command} )
 if [ $? -eq 0 ]; then
