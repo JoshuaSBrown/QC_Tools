ce1978820e850a010e10d8dd6bbdb75348e0d10f
Joshua Scott Brown <joshbro42867@yahoo.com>
2019-04-05 17:38:00 -0600

    Updated release.bash file

--- scripts/release.bash
+++ scripts/release.bash
@@ -1,28 +1,14 @@
 #!/bin/bash
 
 directory=$(pwd)
-# CENTOS
-cd ${directory}/release_distros/centos7
-pwd
-ls
-cp ${directory}/release_distros/package_release.bash ${directory}/release_distros/centos7/
-docker build ${directory}/release_distros/centos7 -t catnipcentos7
-id=$(docker create catnipcentos7)
-docker cp $id:TAR .
-rm ${directory}/release_distros/centos7/package_release.bash
-
-# FEDORA
-#cd ${directory}/release_distros/fedora28
-#pwd
-#ls
-#docker build ${directory}/release_distros/fedora28 -t catnipfedora28
-#id=$(docker create catnipfedora28)
-#docker cp $id:QC_Tools/TAR .
-#
-## UBUNTU
-#cd ${directory}/release_distros/ubuntu18
-#pwd
-#ls
-#docker build ${directory}/release_distros/ubuntu18 -t catnipubuntu18
-#id=$(docker create catnipubuntu18)
-#docker cp $id:QC_Tools/TAR .
+distros="centos7 fedora28 ubuntu18"
+for distro in $distros; do
+  cd ${directory}/release_distros/${distro}
+  pwd
+  ls
+  cp ${directory}/release_distros/package_release.bash ${directory}/release_distros/${distro}/
+  docker build ${directory}/release_distros/${distro} -t catnip${distro}
+  id=$(docker create catnip${distro})
+  docker cp $id:TAR .
+  rm ${directory}/release_distros/${distro}/package_release.bash
+done 
