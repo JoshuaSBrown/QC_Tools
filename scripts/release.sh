6c5307b3a46f04674ec0ef82646433dfed007c0c
Joshua Scott Brown <joshbro42867@yahoo.com>
2019-04-05 15:32:31 -0600

    Tweaking Dockerfiles for each distro

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,26 @@
-!#/bin/bash
+#!/bin/bash
 
-docker exec fedora:28 release_distros/release_fedora.sh
-docker exec ubuntu:bionic release_distros/release_ubuntu.sh
-docker exec centos:7 release_distros/release_centos.sh
+directory=$(pwd)
+# CENTOS
+cd ${directory}/release_distros/centos7
+pwd
+ls
+docker build ${directory}/release_distros/centos7 -t catnipcentos7
+id=$(docker create catnipcentos7)
+docker cp $id:QC_Tools/TAR .
+
+# FEDORA
+cd ${directory}/release_distros/fedora28
+pwd
+ls
+docker build ${directory}/release_distros/fedora28 -t catnipfedora28
+id=$(docker create catnipfedora28)
+docker cp $id:QC_Tools/TAR .
+
+# UBUNTU
+cd ${directory}/release_distros/ubuntu18
+pwd
+ls
+docker build ${directory}/release_distros/ubuntu18 -t catnipubuntu18
+id=$(docker create catnipubuntu18)
+docker cp $id:QC_Tools/TAR .
