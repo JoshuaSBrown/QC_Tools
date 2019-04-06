6d51fb7c6c1b53e2c3e7dafe2d03614638c3b88b
Joshua Scott Brown <joshbro42867@yahoo.com>
2019-04-05 22:56:19 -0600

    CHECKPOINT

--- scripts/release.bash
+++ scripts/release.bash
@@ -11,4 +11,20 @@ for distro in $distros; do
   id=$(docker create catnip${distro})
   docker cp $id:TAR .
   rm ${directory}/release_distros/${distro}/package_release.bash
-done 
+done
+
+mkdir temp_release_zip
+cd temp_release_zip
+git clone https://github.com/JoshuaSBrown/QC_Tools.git
+rm -rf QC_Tools/.git
+rm -rf QC_Tools/.clang-format
+rm -rf QC_Tools/travis
+rm -rf QC_Tools/.travis.yml
+rm -rf QC_Tools/docs
+rm -rf QC_Tools/scripts
+MAJOR_VERSION=$(cat QC_Tools/CMakeLists.txt | grep VERSION_MAJOR | grep -oP '\(\K[^\)]+' | awk '{print $2}')
+MINOR_VERSION=$(cat QC_Tools/CMakeLists.txt | grep VERSION_MINOR | grep -oP '\(\K[^\)]+' | awk '{print $2}')
+zip -r calc_J_${MAJOR_VERSION}.${MINOR_VERSION}_source.zip QC_Tools
+tar -zcvf calc_J_${MAJOR_VERSION}.${MINOR_VERSION}_source.tar.gz QC_Tools
+rm -rf QC_Tools
+
--- scripts/release_distros/package_release.bash
+++ scripts/release_distros/package_release.bash
@@ -7,6 +7,7 @@ DISTRO=$(lsb_release -a | grep ID | awk '{print $3}')
 DISTRO_VERSION=$(lsb_release -a | grep Release | awk '{print $2}')
 tar_file=$(echo calc_J_${VERSION}_${DISTRO}_${DISTRO_VERSION}_${FORMAT}.tar.gz)
 echo "tar file name "${tar_file}
+ls /QC_Tools/build
 tar -zcvf "${tar_file}" QC_Tools/build/calc_J                                        
 mkdir TAR                                                                   
 mv *tar.gz TAR/    
