04d36d7a2cb06c52e44b195e8bbfb4f3638bc485
Joshua Scott Brown <joshbro42867@yahoo.com>
2019-04-06 09:41:53 -0600

    Renamed file

---
+++ travis/test_suite_install.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Pulls GAUSSIANFILES.zip from public google drive and unzips
+if [ -z "$1" ]; then
+  DOWNLOAD_TO="."
+elif [ -d "$1" ]; then
+  DOWNLOAD_TO=$1
+else
+  echo "Directory does not exist "$1
+  exit 1
+fi
+
+pip install --user --upgrade pip
+pip install --user gdown
+gdown https://drive.google.com/uc?id=1rCsj_jpMyE0S0cokFJDyBSA0aPNiIHNb
+
+unzip "GAUSSIANFILES.zip"
+#cleanup
+rm GAUSSIANFILES.zip
+
+if [ -d "${DOWNLOAD_TO}/GAUSSIANFILES" ]; then
+  rm -rf ${DOWNLOAD_TO}/GAUSSIANFILES
+fi
+mv GAUSSIANFILES $DOWNLOAD_TO
+
+
